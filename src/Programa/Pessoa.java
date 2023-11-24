a229ab81b5808b6a05966edae9d5bce11515db74
eduardocesarmaia13 <eduardocesarmaia13@gmail.com>
2023-11-24 10:41:23 -0300

    Getters and Setters

--- src/Programa/Pessoa.java
+++ src/Programa/Pessoa.java
@@ -13,4 +13,25 @@ public class Pessoa {
         this.email = email;
     }
 
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getCpf(){
+        return cpf;
+    }
+    public void setCpf(String cpf){
+        this.cpf = cpf;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+    public void setEmail(String email){
+        this.email = email;
+    }
+
 }
