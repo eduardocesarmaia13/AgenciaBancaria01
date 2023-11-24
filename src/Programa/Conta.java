bafd620e3f7aa82d8b4466cfbc3d7f45ecd114d8
eduardocesarmaia13 <eduardocesarmaia13@gmail.com>
2023-11-24 12:45:13 -0300

    Classe Conta

---
+++ src/Programa/Conta.java
@@ -0,0 +1,11 @@
+package Programa;
+
+public class Conta {
+
+    private static int contadorDeContas = 1;
+
+    private int numeroConta;
+    private Pessoa pessoa;
+    private Double saldo = 0.0;
+
+}
